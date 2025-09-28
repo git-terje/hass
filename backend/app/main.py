0b497e8de4fae7b2097187316ab79da0d73f0438
Terje R <t.ronning@gmail.com>
2025-09-28 12:06:23 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
-from .routes import health
+from .routes import health, products, resellers
+from .database import Base, engine
 
 app = FastAPI()
 
-app.include_router(health.router, prefix="/api")
+# Opprett tabeller
+Base.metadata.create_all(bind=engine)
+
+# Ruter
+app.include_router(health.router)
+app.include_router(products.router)
+app.include_router(resellers.router)
 
 @app.get("/api")
 def root():
     return {"message": "Backend is running"}
-
