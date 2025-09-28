fd1c34b5364472111be4f6b57ad2cef3101a80fa
Terje R <t.ronning@gmail.com>
2025-09-28 19:08:30 +0200

    Cleanup ha pos

--- addons/ha_pos/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-set -e
-
-bashio::log.info "Starting HA POS..."
-
-# Start uvicorn direkte som PID 1 (ingen underprosesser som kan feile)
-exec uvicorn run:app --host 0.0.0.0 --port 8091
